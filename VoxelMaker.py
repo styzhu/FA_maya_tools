ba917e8b6d6cb8836c182716ceef3fb920b31750
StyZhu <zhu@junyuan.me>
2014-12-10 21:46:03 -0700

    voxel maker version 1.0

--- VoxelMaker.py
+++ VoxelMaker.py
@@ -65,11 +65,9 @@ def NormalizeMesh(mesh):
     for i in range(3):
         if edgeList[i] >= maxLength:
             maxLength = edgeList[i]
-        else:
-            continue
     
-    if maxLength > 1:
-        pm.scale(mesh, mesh.getScale()[0]/(maxLength), mesh.getScale()[1]/(maxLength), mesh.getScale()[2]/(maxLength))
+    # use 'if maxLength > 1:' if only want to decrease the sample
+    pm.scale(mesh, mesh.getScale()[0]/(maxLength), mesh.getScale()[1]/(maxLength), mesh.getScale()[2]/(maxLength))    
     
     pm.makeIdentity(mesh, apply = True, translate=True, rotate=True, scale=True)
 
@@ -144,8 +142,8 @@ class VoxelMaker:
         if i_isGroup ==True:
             if len(self._voxelsList)>1:
                 pm.polyUnite(self._voxelsList, name='V1')
-            pm.polyMergeVertex(distance=0.0)
-            pm.delete(pm.polyInfo(laminaFaces=True))
+                pm.polyMergeVertex(distance=0.0)
+                pm.delete(pm.polyInfo(laminaFaces=True))
             
         pm.delete(self._oriMdl)
         
@@ -189,7 +187,7 @@ class UI:
     _mainWindow = None
     _customVoxelSelected = False
     _importSelected = False
-    _seperated = False
+    _separated = False
     _density = 1
 
     #UI for this tool
@@ -213,7 +211,8 @@ class UI:
         pm.menuItem(label='Save Settings')
         pm.menuItem(label='Reset Settings')
         pm.menu(label='Help')
-        pm.menuItem(label='Design Doc', command=self.goDocLink)    
+        pm.menuItem(label='Wiki', command=self.goDocLink)    
+        pm.menuItem(label='Report Issues', command=self.goBugLink)   
         
         # add layouts
         colLayout = pm.columnLayout(adj=True, parent=self._mainWindow)
@@ -229,10 +228,6 @@ class UI:
         
         formLayout_cube.attachForm(cvRButton, 'top', topOffset)
         formLayout_cube.attachForm(cvRButton, 'left', leftOffset)
-        """
-        formLayout_cube.attachControl(testCtrlGrp, 'top', topOffset, cvRButton)
-        formLayout_cube.attachForm(testCtrlGrp, 'left', -100) # TODO
-        """
         
         # frame 2
         frameLayout_custom = pm.frameLayout('custom', label='Custom Voxel Settings', borderStyle='etchedOut', parent=colLayout)
@@ -245,7 +240,7 @@ class UI:
         importRButton = pm.radioButton(label='Import', parent=formLayout_custom_1, enable=self._customVoxelSelected, sl=self._importSelected)
         importField = pm.textFieldButtonGrp( label='Path', text=self._importFilePath, buttonLabel='Find',
                                              buttonCommand=self.getImportFilePath, parent=formLayout_custom_1, 
-                                             enable=self._customVoxelSelected ) #TODO
+                                             enable=self._customVoxelSelected )
         insceneRButton = pm.radioButton(label='In Scene: Select the profile first, the sample object secondly', parent=formLayout_custom_1, enable=self._customVoxelSelected,
                                         changeCommand=self.changeImportSelected, sl=not self._importSelected)
         
@@ -272,12 +267,12 @@ class UI:
         formLayout_density.attachForm(densityIntSlider, 'left', leftOffset-100)
 
         # frame 4
-        frameLayout_seperate = pm.frameLayout(label='Is Result Seperated', borderStyle='etchedOut', parent=colLayout)
+        frameLayout_seperate = pm.frameLayout(label='Is Result separated', borderStyle='etchedOut', parent=colLayout)
         formLayout_seperate = pm.formLayout(parent=frameLayout_seperate)
         
         isSeperate = pm.radioCollection('IsSeperate')
-        ySepRButton = pm.radioButton('Yes', label='Yes', parent=formLayout_seperate, changeCommand=self.changeSeperated, sl=self._seperated)
-        nSepRButton = pm.radioButton('No', label='No', parent=formLayout_seperate, sl=not self._seperated)
+        ySepRButton = pm.radioButton('Yes', label='Yes', parent=formLayout_seperate, changeCommand=self.changeseparated, sl=self._separated)
+        nSepRButton = pm.radioButton('No', label='No', parent=formLayout_seperate, sl=not self._separated)
 
         formLayout_seperate.attachForm(ySepRButton, 'top', topOffset)
         formLayout_seperate.attachForm(ySepRButton, 'left', leftOffset)
@@ -301,8 +296,8 @@ class UI:
         self.__init__()
         
         
-    def changeSeperated(self, *args):
-        self._seperated = not self._seperated
+    def changeseparated(self, *args):
+        self._separated = not self._separated
         self.__init__()
         
         
@@ -345,7 +340,12 @@ class UI:
     
     def goDocLink(self, *args):
         """go to document/help"""
-        pm.launch(web='https://docs.google.com/document/d/1rGMKBQ3gdmisncroBUUkPibmySuQh--DR_MUgqCfOlY/edit?usp=sharing')
+        pm.launch(web='https://github.com/styzhu/FA_maya_tools/wiki/Voxel-Maker-Tool')
+    
+    
+    def goBugLink(self, *args):
+        """let user report bug"""
+        pm.launch(web='https://github.com/styzhu/FA_maya_tools/issues')
     
     
     def apply(self, i_voxelType, i_meshPath, i_isClose, *args):
@@ -374,7 +374,7 @@ class UI:
         
         if i_voxelType.getSelect() == 'custom_voxel':
             if self._importSelected:
-                print pm.importFile(i_meshPath, returnNewNodes=True)
+                print "Import File: "+str(pm.importFile(i_meshPath, returnNewNodes=True))
                 transform = pm.importFile(i_meshPath, returnNewNodes=True)[-2]
                 NormalizeMesh(transform)
             else:
@@ -382,7 +382,7 @@ class UI:
                 NormalizeMesh(transform)
         else:
             transform = pm.polyCube(w=voxelSize, h=voxelSize, d=voxelSize)
-        if self._seperated == True:
+        if self._separated == True:
             vm.createShape(False, transform)
         else:
             vm.createShape(True, transform)
